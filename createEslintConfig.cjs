d00e0cd75005d6094ae51a314072c68362677a57
James Browning <james.browning@canva.com>
2019-09-11 17:34:05 +1000

    .

--- createEslintConfig.cjs
+++ createEslintConfig.cjs
@@ -11,6 +11,7 @@ module.exports = function createEslintConfig({ project="./tsconfig.json" }={}) {
         parserOptions: {
             ...parserOptions,
             project,
+            extraFileExtensions: [".cjs"],
         },
         plugins,
         overrides,
