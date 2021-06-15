1d439621f03dec5d1eec99abc4bbb6f865e0113d
James Browning <thejamesernator@gmail.com>
2021-06-15 18:35:55 +1200

    .

--- createEslintConfig.cjs
+++ createEslintConfig.cjs
@@ -11,7 +11,7 @@ module.exports = function createEslintConfig({ project="./tsconfig.json" }={}) {
         parserOptions: {
             ...parserOptions,
             project,
-            extraFileExtensions: [".cjs"],
+            extraFileExtensions: [".cjs", ".mjs"],
         },
         plugins,
         overrides,
